7ab08e14e3e9152d7edd23df684bb8c064f5abbe
petrov bogdan <bogdan_emdetei_petrov@mail.ru>
2022-02-09 15:13:22 +0300

    сделал адаптив для хедера главное

--- source/js/script.js
+++ source/js/script.js
@@ -5,8 +5,22 @@ $(document).ready(function(){
     slidesToScroll:1,
     autoplay:true,
   });
-
+//кнопка
   $('.header__nav-btn').on('click',function(){
     $('.list,.header__nav-btn').toggleClass('list--activ');
+  });
+  //меню и картинка при 330пх
+  $('.header__nav-btn').on('click',function(){
+    $('.header__inner-title').toggleClass('menu__bottom');
+  });
+  $('.header__nav-btn').on('click',function(){
+    $('.header').toggleClass('header__bottom-img');
+  });
+  //меню и картинка при 900пх
+  $('.header__nav-btn').on('click',function(){
+    $('.header__inner-title-text').toggleClass('header__inner-title-text-bottom');
+  });
+  $('.header__nav-btn').on('click',function(){
+    $('.header').toggleClass('header__bottom-img-big');
   });
 });
